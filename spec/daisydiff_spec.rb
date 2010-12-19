a140c8b8fb2ec9049917a5ea05de74ffd3b61d36
Howard Yeh <hayeah@gmail.com>
2010-12-18 23:35:43 -0800

    sample output in spec

--- spec/daisydiff_spec.rb
+++ spec/daisydiff_spec.rb
@@ -2,6 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Daisydiff" do
   it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+    html1 = <<-HTML
+hello world
+bye world
+    HTML
+    html2 = <<-HTML
+<b>hello world</b>
+    HTML
+    puts DaisyDiff.strings(html1, html2)
+    fail "maybe test it?"
   end
 end
+
+
+
+
+
+
+
